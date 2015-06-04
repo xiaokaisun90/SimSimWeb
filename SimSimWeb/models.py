8f5c7e452dd52eac3ecca651fdd57171bcb99f68
shijie rao <shijiera@andrew.cmu.edu>
2015-06-04 11:38:02 -0700

    model has been changed

--- SimSimWeb/models.py
+++ SimSimWeb/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserState(models.Model):
+    user_state_id = models.IntegerField(primary_key=True)
+    user_state_type = models.CharField(max_length=50)
+
+
+class UserRoleTypes(models.Model):
+    user_role_type_id = models.IntegerField(primary_key = True)
+    user_role_type = models.CharField(max_length=64)
+
+
 class UserInfo(models.Model):
     #user_num is the user_id to avoid the clash with built-in user
     user_num = models.IntegerField(primary_key=True)
-    user = models.OneToOneField(User)
+    user_id = models.OneToOneField(User, primary_key = True)
     primary_mobile_number = models.CharField(max_length=50)
     is_mobile_verified = models.CharField(max_length=1, null=True)
     country = models.CharField(max_length=50, null=True)
@@ -13,48 +23,11 @@ class UserInfo(models.Model):
     last_sign_in = models.DateTimeField(null=True)
     is_web_registered = models.CharField(max_length=1, null=True)
     # possible foreign key
-    user_state_id = models.IntegerField(null=True)
+    user_state_id = models.ForeignKey(UserState)
 
-class Properties(models.Model):
-    property_id = models.IntegerField(primary_key=True)
-    description = models.TextField()
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_length=64)
-    zipcode = models.CharField(max_length=24)
-    state = models.CharField(max_length=64)
-    country = models.CharField(max_length=64)
-    is_visible_to_guests = models.CharField(max_length=1)
-
-class Locks(models.Model):
-    lock_id = models.IntegerField(primary_key=True)
-    lock_purpose = models.CharField(max_length=64)
-    is_lock_configured = models.BooleanField(default=False)
-    description = models.TextField()
-    is_lock_active = models.BooleanField(default=False)
-    lock_secret = models.CharField(max_length=255)
-    is_locked = models.BooleanField(default=False)
-    subscription_expiry_date = models.DateTimeField()
-    subscription_last_paid_date = models.DateTimeField()
-    subscription_expiry_amount = models.IntegerField()
-    subscription_plan_id = models.IntegerField()
-    family_count = models.IntegerField()
-    guest_count = models.IntegerField()
-    lock_power = models.FloatField()
-    lock_start_angle = models.FloatField()
-    lock_end_angle = models.FloatField()
-
-class UserPropertyLocks(models.Model):
-    user_id = models.IntegerField()
-    property_lock_id = models.IntegerField()
-    user_role_type_id = models.IntegerField()
-
-class PropertyLocks(models.Model):
-    property_lock_id = models.IntegerField()
-    property_id = models.IntegerField()
-    lock_id = models.IntegerField()
 
 class UserProfile(models.Model):
-    user_id = models.ForeignKey(User, primary_key=True)
+    user_id = models.ForeignKey(UserInfo, primary_key=True)
     avatar_url = models.CharField(max_length=300)
     data_of_birth = models.DateTimeField()
     gender = models.CharField(max_length=1)
@@ -66,43 +39,80 @@ class UserProfile(models.Model):
     zipcode = models.CharField(max_length=50)
     search_engine_visibility = models.CharField(max_length=1)
 
+
+class SubscriptionPlans(models.Model):
+    subscription_plan_id = models.IntegerField(primary_key = True)
+    subscription_plan = models.CharField(max_length=50)
+
+
+class Properties(models.Model):
+    property_id = models.IntegerField(primary_key=True)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=64)
+    zipcode = models.CharField(max_length=24)
+    state = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    is_visible_to_guests = models.CharField(max_length=1)
+
+
+class Locks(models.Model):
+    lock_id = models.IntegerField(primary_key=True)
+    lock_purpose = models.CharField(max_length=64)
+    is_lock_configured = models.BooleanField(default=False)
+    description = models.TextField()
+    is_lock_active = models.BooleanField(default=False)
+    lock_secret = models.CharField(max_length=255)
+    is_locked = models.BooleanField(default=False)
+    subscription_expiry_date = models.DateTimeField()
+    subscription_last_paid_date = models.DateTimeField()
+    subscription_last_paid_amount = models.IntegerField()
+    subscription_plan_id = models.ForeignKey(SubscriptionPlans)
+    family_count = models.IntegerField()
+    guest_count = models.IntegerField()
+    lock_power = models.FloatField()
+    lock_start_angle = models.FloatField()
+    lock_end_angle = models.FloatField()
+
+
+class PropertyLocks(models.Model):
+    property_lock_id = models.IntegerField(primary_key=True)
+    property_id = models.ForeignKey(Properties)
+    lock_id = models.ForeignKey(Locks)
+
+
+class UserPropertyLocks(models.Model):
+    user_id = models.ForeignKey(UserInfo)
+    property_lock_id = models.ForeignKey(PropertyLocks)
+    user_role_type_id = models.ForeignKey(UserRoleTypes)
+
+
 class UserIntermediateData(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(UserInfo, primary_key=True)
     intermediate_mobile_number = models.CharField(max_length=50)
     intermediate_email_address = models.CharField(max_length=100)
 
+
 class UserNotificationPreferences(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(UserInfo, primary_key=True)
     lock_unlock_notification_choice = models.BooleanField(default=False)
     unlocked_5mins_notification_choice = models.BooleanField(default=False)
     guest_request_access_notification_choice = models.BooleanField(default=False)
     wifi_disconnected_notification_choice = models.BooleanField(default=False)
 
-class UserState(models.Model):
-    user_state_id = models.IntegerField(primary_key=True)
-    user_state_type = models.CharField(max_length=50)
 
-class UserRoleTypes(models.Model):
-    user_role_type_id = models.IntegerField()
-    user_role_type = models.CharField(max_length=64)
+class LockActivityType(models.Model):
+    lock_activity_type_id = models.IntegerField(primary_key = True)
+    lock_activity_type = models.CharField(max_length=64)
 
-class SubscriptionPlans(models.Model):
-    subscription_plan_id = models.IntegerField()
-    subscription_plan = models.CharField(max_length=50)
 
 class LockActivity(models.Model):
-    lock_id = models.IntegerField()
-    lock_activity_type_id = models.IntegerField()
+    lock_activity_id = models.IntegerField(primary_key = True)
+    lock_id = models.ForeignKey(Locks)
+    lock_activity_type_id = models.ForeignKey(LockActivityType)
     lock_activity_time_stamp = models.DateTimeField()
     user_id = models.IntegerField()
 
-class LockActivityType(models.Model):
-    lock_activity_id = models.IntegerField()
-    lock_activity_type = models.CharField(max_length=64)
-
-class UserAccessRequest(models.Model):
-    user_id = models.IntegerField()
-    request_id = models.IntegerField()
 
 class GuestAccessRequestQueue(models.Model):
     request_id = models.IntegerField(primary_key=True)
@@ -115,10 +125,15 @@ class GuestAccessRequestQueue(models.Model):
     access_times = models.IntegerField()
 
 
+class UserAccessRequest(models.Model):
+    user_id = models.ForeignKey(UserInfo)
+    request_id = models.ForeignKey(GuestAccessRequestQueue)
+
+
 class ApprovedAccessRequest(models.Model):
     request_id = models.IntegerField(primary_key=True)
     property_lock_id = models.ForeignKey(PropertyLocks)
-    access_given_by_user_id = models.IntegerField()
+    access_given_by_user_id = models.ForeignKey(UserInfo)
     allocated_access_start_time = models.DateTimeField()
     allocated_access_end_time = models.DateTimeField()
     access_request_state_id = models.IntegerField()
